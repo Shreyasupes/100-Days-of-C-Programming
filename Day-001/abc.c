d2b580fcb5616d0a58acc9b0b3b437b2f1e535ff
Shreyasupes <Shreyas.42649@stu.upes.ac.in.com>
2026-09-20 07:07:03 +0000

    Added abc.c and sum.c

---
+++ Day-001/abc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+
+  printf("Enter two numbers:");
+  scanf("%d %d", &a, &b);
+
+  printf("Sum = %d\n",a + b);
+  printf("Difference = %d\n",a - b);
+  printf("Product = %d\n",a * b);
+
+  if (b != 0)
+  {
+    printf("Division = %d\n",a / b);
+  }
+  else
+  {
+    printf("Division is not possible because denominator is zero.\n");
+  }
+
+   return 0;
+}
+
